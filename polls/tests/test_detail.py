3e489080de10a47a6e9429fd3c457fc47b57840a
kinkinkinxd <kinkinkinxd@gmail.com>
2020-10-30 00:36:26 +0700

    fix

--- polls/tests/test_detail.py
+++ polls/tests/test_detail.py
@@ -42,4 +42,4 @@ class QuestionDetailViewTests(TestCase):
         past_question = create_question(question_text='Past Question.', days=-5)
         url = reverse('polls:detail', args=(past_question.id,))
         response = self.client.get(url)
-        self.assertContains(response, past_question.question_text)
+        self.assertEqual(response.status_code, 302)
